40bf6294080f0a13f9dced7098010f24be597bcf
Cameron King <cameron.eric.king@gmail.com>
2020-12-24 01:00:10 -0800

    Make better use of templating.

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
-from flask import Flask, render_template
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+
+def get_custom_url():
+    import os
+    return os.environ.get("CUSTOM_DOMAIN_URL", "")
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
+@app.route("/index")
 def home():
-    return render_template("index.html")
+    subpath = request.path
+    fullpath = f"{get_custom_url()}{subpath}"
+    return render_template(
+        "index.html",
+        url=fullpath,
+        justification="center",
+    )
 
 
-@app.route("/about/")
+@app.route("/about")
 def about():
-    return render_template("about.html")
+    subpath = request.path
+    fullpath = f"{get_custom_url()}{subpath}"
+    return render_template(
+        "about.html",
+        url=fullpath,
+        justification="left",
+    )
+
+@app.errorhandler(404)
+def error(e):
+    fullpath = f"{get_custom_url()}/404"
+    return render_template(
+        "404.html",
+        url=fullpath
+    )
 
 if __name__ == "__main__":
     app.run()
