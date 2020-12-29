713f6143949118afa4622e661be82587c0db5a5a
Cameron King <cameron.eric.king@gmail.com>
2020-12-28 21:28:43 -0800

    Ran pre-commit hooks.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+from flask import Flask, render_template, request
+
 
 def get_custom_url():
     import os
+
     return os.environ.get("CUSTOM_DOMAIN_URL", "")
 
+
 app = Flask(__name__)
+
 
 @app.route("/")
 @app.route("/home")
@@ -16,40 +16,28 @@ app = Flask(__name__)
 def home():
     subpath = request.path
     fullpath = f"{get_custom_url()}{subpath}"
-    return render_template(
-        "index.html",
-        url=fullpath,
-        justification="center",
-    )
+    return render_template("index.html", url=fullpath, justification="center",)
 
 
 @app.route("/about")
 def about():
     subpath = request.path
     fullpath = f"{get_custom_url()}{subpath}"
-    return render_template(
-        "about.html",
-        url=fullpath,
-        justification="left",
-    )
+    return render_template("about.html", url=fullpath, justification="left",)
+
 
 @app.route("/contact")
 def contact():
     subpath = request.path
     fullpath = f"{get_custom_url()}{subpath}"
-    return render_template(
-        "contact.html",
-        url=fullpath,
-        justification="left",
-    )
+    return render_template("contact.html", url=fullpath, justification="left",)
+
 
 @app.errorhandler(404)
 def error(e):
     fullpath = f"{get_custom_url()}/404"
-    return render_template(
-        "404.html",
-        url=fullpath
-    )
+    return render_template("404.html", url=fullpath)
+
 
 if __name__ == "__main__":
     app.run()
